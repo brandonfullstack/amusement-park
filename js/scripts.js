27fbd8af4ba898431c56d6e9994bea79fbaf338d
Brandon Wright <brandonfullstack@gmail.com>
2023-06-13 18:51:06 -0400

    Complete example

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // User Interface Logic
 
-function hideResults() {
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
   document.getElementById("swings").setAttribute("class", "hidden");
   document.getElementById("coaster").setAttribute("class", "hidden");
   document.getElementById("tower").setAttribute("class", "hidden");
@@ -8,10 +9,32 @@ function hideResults() {
 }
 
 window.onload = function() {
+  hideResultsAndError();
+
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
-    hideResults();
+    hideResultsAndError();
     const age = parseInt(document.querySelector("input#age").value);
     const height = parseInt(document.querySelector("input#height").value);
+    
+    if (!age || !height) {
+      document.getElementById("error-message").removeAttribute("class");
+    } else { 
+    // we're evaluating the Tower of Doom ride first
+      if (age >= 12 && height >= 60) {
+      document.getElementById("swings").removeAttribute("class");
+      document.getElementById("coaster").removeAttribute("class");
+      document.getElementById("tower").removeAttribute("class");
+    
+      } else if (age >= 12 || height >= 48) {
+      document.getElementById("swings").removeAttribute("class");
+      document.getElementById("coaster").removeAttribute("class");
+      } else if (age >= 6) {
+      document.getElementById("swings").removeAttribute("class");
+      // Finally, we have our "catch-all" for when a user is less than 6 years old.
+      } else {
+      document.getElementById("sorry").removeAttribute("class");  
+      }
+    }
   };
 };
